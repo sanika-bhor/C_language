fb8990cc2635ed645c4bf6b5254c4340908edf83
sanika-bhor <bhorsanika0239@gmail.com>
2024-09-21 22:08:06 +0530

    making smily face

---
+++ graphics_concept/src/face.c
@@ -0,0 +1,33 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    // Set fill style and color
+    setcolor(YELLOW);
+
+    // creating circle and fill it with
+    // yellow color using floodfill.
+    circle(250, 250, 150);
+    setfillstyle(SOLID_FILL, YELLOW);
+    floodfill(250, 250, YELLOW);
+
+
+
+    
+
+    getch();
+    closegraph();
+    return 0;
+}
+
+// one command for creating exe
+//  g++ -o output  main.c -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
+
+// indetail commands for creating ext
+
+// g++ -I include -c ./src/face.c -o ./build/face.o
+// g++  ./build/face.o  -o ./build/face.exe -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
