f9b94acc2c396d5eb1aaba943ca7f6cc35dd9c0d
sanika-bhor <bhorsanika0239@gmail.com>
2024-09-22 23:36:26 +0530

    bouncing ball struture

--- Bouncing_ball/src/main.c
+++ Bouncing_ball/src/main.c
@@ -1,5 +1,6 @@
 #include <graphics.h>
 #include <conio.h>
+#include <dos.h>
 
 int main()
 {
@@ -7,22 +8,77 @@ int main()
     initgraph(&gd, &gm, "");
 
     // Set fill style and color
-    setcolor(YELLOW);
+    setcolor(WHITE);
 
+    int i;
+    int x = 60;
+    int y = 300;
+    int minY=60;
+    int maxY=520;
+
+    int minX = 60;
+    int maxX = 520;
+
+    for (i = 0; i < 200; i++)
+    {
+        cleardevice();
+        circle(x, y, 40);
+
+        if (y == minY)
+        {
+            x=60;
+            for (int j = 0; j < 200; j++)
+            {
+                cleardevice();
+                circle(x, y, 40);
+                x = x + 20;
+                // y = y + 20;
+                delay(50);
+
+                if (x == maxX)
+                {
+                    for (int j = 0; j < 200; j++)
+                    {
+                        cleardevice();
+                        circle(x, y, 40);
+                        // x = 100;
+                        y = y + 20;
+                        delay(50);
+
+                        if (y == maxY)
+                        {
+                            for (int j = 0; j < 200; j++)
+                            {
+                                cleardevice();
+                                circle(x, y, 40);
+                                x = x - 20;
+                                delay(50);
+
+                                if(x==minX)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+            }
+
+
+        }
+
+            y = y - 20;
+        delay(50);
+    }
     // BALL shape
-    circle(50, 350, 40);
-    setfillstyle(SOLID_FILL, YELLOW);
-    floodfill(250, 250, YELLOW);
+
     getch();
-    
+
     closegraph();
     return 0;
 }
-
-// one command for creating exe
-//  g++ -o output  main.c -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
-
-// indetail commands for creating ext
 
 // g++ -I include -c ./src/main.c -o ./build/main.o
 // g++  ./build/main.o  -o ./build/main.exe -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
