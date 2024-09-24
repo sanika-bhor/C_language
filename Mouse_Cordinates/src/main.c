018943eee1c7ee210de0df20042d84adfed62cd0
sanika-bhor <bhorsanika0239@gmail.com>
2024-09-24 22:28:42 +0530

    mouse graphics

--- Mouse_Cordinates/src/main.c
+++ Mouse_Cordinates/src/main.c
@@ -1,25 +1,28 @@
 // g++ -I include -c ./src/main.c -o ./build/main.o
 // g++  ./build/main.o  -o ./build/main.exe -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
-
 #include <graphics.h>
 #include <stdio.h>
 #include <conio.h>
-
+#include <dos.h>
 int main()
 {
+
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
-    // Set color for drawing
     setcolor(WHITE);
-
-    while (true) // Loop until close the window
+    while (true)
     {
+        cleardevice();
+        // Set text style
+        settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+        char buffer[100];
         int x = mousex();
         int y = mousey();
-        cleardevice();
-
-        printf("Mouse clicked at: x = %d, y = %d\n", x, y);
+        sprintf(buffer, "X=%d, Y=%d", x, y);
+        // Display text at specified coordinates
+        outtextxy(10, 10, buffer);
+        outtextxy(x, y, "*");
         delay(100);
     }
 
@@ -28,3 +31,6 @@ int main()
     closegraph();
     return 0;
 }
+
+// one command for creating exe
+//  g++ -o output  mouse.c -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
